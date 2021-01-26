94b0a7b77f02e34f1d3c6024c6622257bb1367da
Mikhail Shilkov <shilkovmike@gmail.com>
2021-01-26 15:33:32 +0200

    added product pages

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'home.html')
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'product.html')
