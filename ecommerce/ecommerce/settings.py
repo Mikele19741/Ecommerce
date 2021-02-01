f935a8a4ecb04139e929536dd420258b39aa59d5
Mikhail Shilkov <shilkovmike@gmail.com>
2021-02-01 12:07:06 +0200

    finished create shop

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'shop.context_processor.menu_links',
+                'shop.context_processor.counter',
             ],
         },
     },
@@ -129,4 +131,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'static','media')
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
